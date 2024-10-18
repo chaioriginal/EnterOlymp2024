7115eba4471d7c019cdb60b1b40972e290a81d2f
chaioriginal <elijahgrni@outlook.com>
2024-10-18 18:34:26 +0800

    tema

--- chai/karti.py
+++ chai/karti.py
@@ -1,6 +1,24 @@
+def naminal_karti(card: tuple):
+    if card[0].isdigit():
+        return int(card[0])
+    return 10
+def kruchenie(kolodakart:list):
+    k = kolodakart[25:]
+    ok=kolodakart[25:52]
+    y=0
+    for i in range(3):
+        b=naminal_karti(k[0])
+        y+=b
+        k=k[9 - b:]
+    ok.append(k)
+    return ok[y]
+
+
+
+
 a=input()
 koloda_kart=a.split()
 newkoloda_kart=[]
 for  i in koloda_kart:
     newkoloda_kart.append((i[0],i[1]))
-print(newkoloda_kart)
+print(kruchenie(a))
---
+++ chai/voobshehzchto.py
@@ -0,0 +1,15 @@
+def zhisla(spisok:list)-> bool:
+
+    s=set()
+    for i in spisok:
+        if  i in s:            
+            return True
+        s.add(i)
+    return False
+def chislo6(spisok:list,a)->bool:
+    s=set()
+    for i in s:
+        if i in spisok:
+#def sortedchislo6()
+
+
